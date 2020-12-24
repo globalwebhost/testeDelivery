846b24bb27102908af4674059b7c56ab60e55f81
JOSÉ RIBBEIRO DA SILVA <ribanet87@gmail.com>
2020-12-24 15:13:49 -0300

    Removendo Express

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
