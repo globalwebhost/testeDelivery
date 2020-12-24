c0eedc7f103265830c8b3d164f569f9a344275b6
JOSÉ RIBBEIRO DA SILVA <ribanet87@gmail.com>
2020-12-24 14:29:34 -0300

    Adicionando body-parser e alterando Index

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+app.get('/testeDelivery', (req, res) => {
+    var intentName = request.body.queryResult.intent.displayName;
+    
+if (intentName == "intentTest") {
+ 
+
+ response.json({"fulfillmentText" : "Isso aqui é um Teste"});
+ } 
+   
 })
 
 app.listen(3000, () => {console.log("Servidor Rodando")})
