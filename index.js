00401b36037973e0e7fdfe2faef82c37ee2c685b
JOSÉ RIBBEIRO DA SILVA <ribanet87@gmail.com>
2020-12-24 13:53:14 -0300

    Modificando arquivos Index e Pakage

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World');
+    res.send('<h1>Hello World</h1>');
 })
 
 app.listen(3000, () => {console.log("Servidor Rodando")})
